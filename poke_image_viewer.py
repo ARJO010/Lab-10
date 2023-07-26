f3edfa7d0e992d29590ba0dfe6ec3b0337bef374
ARJO010 <133833421+ARJO010@users.noreply.github.com>
2023-07-26 16:15:16 -0400

    changed code

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -9,18 +9,33 @@ Usage:
 from tkinter import *
 from tkinter import ttk
 import os
+import poke_api
+import image_lib
+import ctypes
+import inspect
 
 # Get the script and images directory
+script_name = inspect.getframeinfo(inspect.currentframe()).filename
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
 # TODO: Create the images directory if it does not exist
+if not os.path.isdir(images_dir):
+    os.makedirs(images_dir)
+
 
 # Create the main window
 root = Tk()
 root.title("Pokemon Viewer")
+root.geometry('600x600')
+root.minsize(500,600)
+root.columnconfigure(0,weight=1)
+root.rowconfigure(0,weight=1)
 
 # TODO: Set the icon
+app_id = 'COMP593.PokeImageViewer'
+ctypes.windll.shell32.SetCurrentProcessExplictAppUserModelID(app_id)
+root.iconbitmap(os.path.join(script_dir,'poke_ball.ico'))
 
 # TODO: Create frames
 
